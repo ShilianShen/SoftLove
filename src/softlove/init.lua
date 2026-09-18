6c21ed083e0dc4d48273d0fb713dd3bc28c7e41c
阿沈 <149996372+ShilianShen@users.noreply.github.com>
2026-09-19 07:59:51 +0800

    feat: integrate ntags module into window and init for enhanced functionality

--- src/softlove/init.lua
+++ src/softlove/init.lua
@@ -5,6 +5,7 @@ local softlove = {
 	ui = require("softlove.ui"),
 	locales = require("softlove.locales"),
 	window = require("softlove.window"),
+	ntags = require("softlove.ntags"),
 }
 
 return softlove
--- src/softlove/window.lua
+++ src/softlove/window.lua
@@ -1,3 +1,37 @@
 local window = {}
+local ntags = require("softlove.ntags")
+
+function window.getNode()
+	local node = {
+		tasks = {
+			init = {
+				func = function(self)
+					self.w, self.h = love.graphics.getDimensions()
+				end,
+			},
+			update = {
+				func = function(self)
+					self.w, self.h = love.graphics.getDimensions()
+				end,
+				parents_c = { "init" },
+			},
+		},
+		apis = {
+			resize = {
+				ttag = "update",
+			},
+		},
+	}
+	return node
+end
+
+function window.setCallbacks(graph)
+	love.focus = function(focus) end
+	love.visible = function(visible) end
+	love.resize = function(w, h)
+		graph.nodes[ntags.window].apis.resize()
+	end
+	love.displayrotated = function(displayindex, orientation) end
+end
 
 return window
