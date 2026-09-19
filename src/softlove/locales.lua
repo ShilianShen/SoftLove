fef3777b6c6fad60c7b50f7563c45282e46c80c6
阿沈 <149996372+ShilianShen@users.noreply.github.com>
2026-09-20 00:04:28 +0800

    feat: implement locale management functions for better translation handling

--- src/softlove/locales.lua
+++ src/softlove/locales.lua
@@ -1,3 +1,42 @@
 local locales = {}
 
+local function setCurrent(self, locale)
+	self.current = locale
+end
+
+local function translate(self, key)
+	return self.translater[self.current][key]
+end
+
+local function init(self)
+	self.translater = {}
+	self.current = "en-US"
+	self.translate = translate
+end
+
+local function add(self, locale, file)
+	self.translater[locale] = require(file)
+end
+
+function locales.getNode()
+	local node = {
+		tasks = {
+			init = {
+				func = init,
+			},
+		},
+		apis = {
+			add = {
+				func = add,
+				atag = "writable",
+			},
+			setCurrent = {
+				func = setCurrent,
+				atag = "writable",
+			},
+		},
+	}
+	return node
+end
+
 return locales
